31e216e40987e7b8e872fb3a2fc809c83c4dc0c8
saquib156 <saquibjamal.jamal156@gmail.com>
2021-10-10 16:27:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/news-app">
+    <Router basename="/">
       <div className="App">
         <Header />
         <Switch>
